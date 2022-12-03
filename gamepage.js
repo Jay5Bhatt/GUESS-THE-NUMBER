38f7cb416b16309a46ec149db4ddfaf741aded2a
Jay5Bhatt <107789701+Jay5Bhatt@users.noreply.github.com>
2022-12-03 18:31:21 +0530

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,32 @@
+// random value generated
+  var y = Math.floor(Math.random() * 10 + 1);
+
+// counting the number of guesses
+var guess = 1;
+// made for correct Guess
+
+  
+// function for number guessed by user 
+var x = document.getElementById("guessField").value;
+
+if(x == y) 
+{
+    alert("CONGRATULATIONS!!!!"+playername+"YOU GUESSED IT RIGHT IN"+ guess + "GUESS");
+    guess=1;
+}
+
+else if(x > y)
+
+{
+    guess++;
+    alert("OOPS! TRY A SMALLER NUMBER");
+}
+else
+{
+    guess++;
+    alert("OOPS! TRY A GREATER NUMBER");
+}
+
+function playAgain() {
+    y = Math.floor(Math.random() * 10 + 1);
+}
